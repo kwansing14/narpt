fc89b31ce415b1ea1c53e66e88bdb0c176f3ee03
kwansing14 <kwansing@hotmail.sg>
2023-06-25 23:19:31 +0800

    exporting use transition

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Testing
+testing
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { TransitionDiv } from './components/TransitionDiv';
 export { TransitionLayout } from './components/TransitionLayout';
 export { TransitionLink } from './components/TransitionLink';
+export { useTransition } from './components/TransitionLayout';
