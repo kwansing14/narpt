3b5825772e2494fe93b9f078dd75da1b5b8a551e
kwansing14 <kwansing@hotmail.sg>
2023-07-12 23:00:29 +0800

    add analytics

---
+++ website/app/api/loglib/route.ts
@@ -0,0 +1,9 @@
+import { createServerRoutes } from '@loglib/next';
+import { prismaAdapter } from '@loglib/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const db = new PrismaClient();
+
+export const { POST, GET } = createServerRoutes({
+  adapter: prismaAdapter(db),
+});
